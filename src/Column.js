78ea538e62578e02270389d2fa883f844f317fad
ASUS <Yehor Kholodiuk>
2022-02-07 14:55:24 +0200

    initial commit

--- src/Column.js
+++ src/Column.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Grid} from "@mui/material";
-
+import Paper from '@mui/material/Paper';
 const Column = (props) => {
     return (
 
         <Grid item xs={3}>
-        <ul>
+            <Paper>
+                <ul>
 
-            {props.tasks.filter(task => task.status === props.status).map(task => <li key={task.id}>{task.title} </li>)}
-        </ul>
+                    {props.tasks.filter(task => task.status === props.status).map(task => <li key={task.id}>{task.title} </li>)}
+                </ul>
+            </Paper>
+
             </Grid>
     );
 };
